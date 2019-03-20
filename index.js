22690e2f21a50b1d9be827a2f9dff3dac87265de
Tropesage <Tropeknight@gmail.com>
2019-03-19 20:38:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -21,4 +21,8 @@ app.get('/half/:x', function(req, resp) {
   resp.send(`${parseInt(req.params.x) / 2}`)
 })
 
-app.listen(3000)
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
